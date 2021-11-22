45565b6c8ac84a67e0ff2f138c6b270deb2ba7dd
mzahid3 <mzahid3@hawk.iit.edu>
2021-11-22 16:47:51 -0600

    Added Login Activity for hard coded login will be putting firebase later

--- app/src/main/java/com/example/cakeorderingapp/LoginActivity.java
+++ app/src/main/java/com/example/cakeorderingapp/LoginActivity.java
@@ -1,4 +1,58 @@
 package com.example.cakeorderingapp;
 
-public class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    int triesCount = 3;//this is going to handle the tries count
+    StringBuilder info = new StringBuilder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void loginUserAccount() {
+
+        System.out.print("Muhammad N Zahid");
+        String uname = "mzahid", pass = "password";//credentials that are going to be used
+        EditText name, word;
+        name = findViewById(R.id.idUserName);
+        word = findViewById(R.id.idPassword);
+
+
+        if (uname.equals(name.getText().toString()) && pass.equals(word.getText().toString())
+                && triesCount != 0) {
+// if login info is correct then move forward
+            Toast.makeText(this, "Successful Login. Redirecting...",
+                    Toast.LENGTH_LONG).show();
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+        // if login info is wrong then decrease the count size
+        else if ((!uname.equals(name.getText().toString()) || !pass.equals(
+                word.getText().toString())) && triesCount >= 1) {
+            triesCount = triesCount - 1;
+
+            info.append("User " + name.getText() + "has " + "\n" + triesCount + "tries left\n");
+            showMessage(info.toString());
+        }
+
+        else {
+            //locked message if the tries are done
+            Toast.makeText(this, "Account locked", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
